d6cd427b360817bcfc456b8da02266127140c285
Sudipto Kumar Shil <sudiptoshil@outlook.com>
2024-09-06 22:31:41 +0600

    Product detail page has been added

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const loadProduct = (category = null) => {
                             <div class="card-body">
                                 <h5 class="card-title">${element.title.split(' ').slice(0, 4).join(' ')}</h5>
                                 <p class="card-text">${element.price}</p>
-                                <div class="text-center"><a href="#" class="btn btn-secondary btn-sm">View</a></div>
+                                <div class="text-center"><button type="button" onclick="loadProductDetail(${element.id})" class="btn btn-secondary btn-sm">View</button></div>
                             </div>
                         </div>
                     </div>`
@@ -41,6 +41,47 @@ const loadProduct = (category = null) => {
             }, 1000);
         })
         .catch(error => console.error('Error:', error));
+}
+
+const loadProductDetail = (product_id) => {
+    let loader = document.getElementById('loader');
+    loader.style.display = "block";
+    
+    fetch(`https://api.allorigins.win/raw?url=https://fakestoreapi.com/products/${product_id}`)
+        .then(res => res.json())
+        .then(json => {
+            loadProductHtml(json)
+
+        }).catch(error => console.error('Error:', error));
+}
+
+
+const loadProductHtml = (data) => {
+    let html = '';
+    html += `
+          <div class="container mt-4">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="${data.image}" alt="${data.image}" class="product-image img-fluid">
+                </div>
+
+                <div class="col-md-6 product-details">
+                    <h2 class="product-title">${data.title}</h2>
+                    <p class="text-muted">Category: ${data.category}</p>
+                    <h4 class="product-price text-danger">$${data.price}</h4>
+                    <div class="product-description">
+                        <h5>Description</h5>
+                        <p>${data.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+    setTimeout(() => {
+        document.getElementById('product_info').innerHTML = html;
+        $('#exampleModal').modal('show');
+        loader.style.display = "none";
+    }, 500);
+
 }
 
 loadCategory();
